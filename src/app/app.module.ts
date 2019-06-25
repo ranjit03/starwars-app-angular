f010d13599fa601cab922431c39fa802a66395e7
ranjit <ranjitendurthi03@gmail.com>
2019-06-25 12:07:21 +1200

    styles added to tabs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListsComponent } from './lists/lists.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ListsComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
